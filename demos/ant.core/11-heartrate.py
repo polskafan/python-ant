1bd836ae9fcd8cb3f050851ffad2aed7dcf34174
Matt Hughes <matt.hughes@calgaryscientific.com>
2017-05-01 06:57:37 -0600

    print montior information for pair in demo program

--- demos/ant.core/11-heartrate.py
+++ demos/ant.core/11-heartrate.py
@@ -23,11 +23,15 @@ hr = HeartRate(antnode)
 #hr = HeartRate(antnode, 23359, 1)
 #hr = HeartRate(antnode, 21840, 81)
 
+monitor = None
 while True:
     try:
         time.sleep(1)
         if hr.state == STATE_RUNNING:
             print "Computed heart rate: {}".format(hr.computed_heart_rate)
+            if monitor is None:
+                monitor = hr.detected_device
+                print "Detect monitor device number: %d, transmission type: %d" % monitor
         if hr.state == STATE_CLOSED:
             print "Channel closed, exiting."
             break
