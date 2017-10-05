8e160e3128cf3406751b336e739520fffe826654
David Hari <d-hari@hotmail.com>
2017-10-05 21:58:14 +1100

    Added ability to get and set crank length.

--- src/ant/plus/power.py
+++ src/ant/plus/power.py
@@ -29,14 +29,19 @@
 
 from struct import Struct
 
+from ant.core.message import *
 from .plus import DeviceProfile
 
 
+CALIBRATION_PAGE = 0x01
+PARAMETERS_PAGE = 0x02
 POWER_ONLY_PAGE = 0x10
 WHEEL_TORQUE_PAGE = 0x11
 CRANK_TORQUE_PAGE = 0x12
 TORQUE_AND_PEDAL_PAGE = 0x13
 CRANK_TORQUE_FREQ_PAGE = 0x20
+
+CRANK_PARAMETER_SUBPAGE = 0x01
 
 
 class BicyclePower(DeviceProfile):
@@ -46,7 +51,7 @@ class BicyclePower(DeviceProfile):
     deviceType = 0x0B
     name = 'Bicycle Power'
 
-    def __init__(self, node, network, callbacks=None):
+    def __init__(self, node, network, callbacks=None, maxQueryTries=10):
         """
         :param node: The ANT node to use
         :param network: The ANT network to connect on
@@ -55,9 +60,13 @@ class BicyclePower(DeviceProfile):
                 `BicyclePower` also has the following:
                 'onPowerData'
                 'onTorqueAndPedalData'
+        :param maxQueryTries: When querying the device for a parameter (such as crank length),
+                this specifies the number of messages to wait for a response before giving up.
         """
         super(BicyclePower, self).__init__(node, network, callbacks)
 
+        self.maxQueryTries = maxQueryTries
+        self.numQueryTries = 0
         self.eventCount = None
         self.pedalDifferentiation = False  # Whether the device can tell the difference between left and right pedals
         self.pedalPowerRatio = None
@@ -76,6 +85,91 @@ class BicyclePower(DeviceProfile):
             TORQUE_AND_PEDAL_PAGE: Struct('<xBBBBBxx'),
             CRANK_TORQUE_FREQ_PAGE: Struct('<xxxxxxxxx')
         }
+
+    def open(self, channelId=None, searchTimeout=30):
+        self.numQueryTries = 0
+        if 'onCrankLengthSuccess' in self.callbacks:
+            del self.callbacks['onCrankLengthSuccess']
+        if 'onCrankLengthTimeout' in self.callbacks:
+            del self.callbacks['onCrankLengthTimeout']
+        super(BicyclePower, self).open(channelId, searchTimeout)
+
+    def requestParameter(self, paramSubPage):
+        """
+        Queries a parameter from the device.
+        """
+        payload = bytes([
+            0x46,               # Common Page 70: Request Data Page
+            0xFF, 0xFF,         # Reserved
+            paramSubPage,       # Sub-page
+            0xFF,               # Invalid
+            self.maxQueryTries, # Number of times to transmit requested page
+            0x02,               # Page 2 (get/set parameters)
+            0x01                # Command type: request data page
+        ])
+        request = ChannelAcknowledgedDataMessage(data=payload)
+        self.channel.send(request)
+
+    def getCrankLength(self, onSuccess=None, onTimeout=None):
+        """
+        Queries the crank length from the device. The `callback` function is called when
+        the device responds with the parameter value.
+        :param onSuccess: Function or lambda which called when the device responds with the value.
+        :param onTimeout: Called if response does not come within `self.maxQueryTries` number of messages.
+        """
+        if 'onCrankLengthSuccess' not in self.callbacks and 'onCrankLengthTimeout' not in self.callbacks:
+            if onSuccess is not None:
+                self.callbacks['onCrankLengthSuccess'] = onSuccess
+            if onTimeout is not None:
+                self.callbacks['onCrankLengthTimeout'] = onTimeout
+            self.numQueryTries = 0
+            self.requestParameter(CRANK_PARAMETER_SUBPAGE)
+
+    def setCrankLength(self, value):
+        """
+        Sets the crank length on the device.
+        """
+        rawCrankValue = int((value - 110.0) / 0.5)
+        payload = bytes([
+            0x02,               # Data Page: Get/Set Bicycle Parameters
+            0x01,               # Sub-page: Crank Parameters
+            0xFF, 0xFF,         # Reserved
+            rawCrankValue,
+            0x00,               # Sensor Status, read-only
+            0x00,               # Sensor Capabilities, read-only
+            0xFF                # Reserved
+        ])
+        request = ChannelAcknowledgedDataMessage(data=payload)
+        self.channel.send(request)
+
+    def process(self, msg, channel):
+        # If we have a callback waiting for a parameter, check if the message has come yet
+        crankLengthCallback = self.callbacks.get('onCrankLengthSuccess')
+        if crankLengthCallback:
+            if self.numQueryTries >= self.maxQueryTries:
+                # Give up if we've waited for too long, and call the timeout callback
+                self.numQueryTries = 0
+                del self.callbacks['onCrankLengthSuccess']
+                timeoutCallback = self.callbacks.get('onCrankLengthTimeout')
+                if timeoutCallback:
+                    del self.callbacks['onCrankLengthTimeout']
+                    timeoutCallback()
+            else:
+                # Check if it's the parameter response message
+                if isinstance(msg, ChannelBroadcastDataMessage)\
+                        and msg.data[0] == PARAMETERS_PAGE\
+                        and msg.data[1] == CRANK_PARAMETER_SUBPAGE:
+                    rawCrankValue = msg.data[4]
+                    actualLength = None if rawCrankValue == 0xFF else (rawCrankValue * 0.5 + 110.0)
+                    # Could also get sensor status and capabilities from bytes 5 and 6
+                    self.numQueryTries = 0
+                    del self.callbacks['onCrankLengthSuccess']
+                    crankLengthCallback(actualLength)
+                else:
+                    super(BicyclePower, self).process(msg, channel)
+            self.numQueryTries += 1
+        else:
+            super(BicyclePower, self).process(msg, channel)
 
     def processData(self, data):
         page = data[0]
